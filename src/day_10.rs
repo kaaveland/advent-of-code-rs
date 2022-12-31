73765aefe2980338f35e19ee634208c6d23ac209
Robin Kåveland <robin.kaveland@sikt.no>
2022-12-31 19:00:22 +0100

    Solve day 10 both parts

---
+++ src/day_10.rs
@@ -0,0 +1,125 @@
+use anyhow::Result;
+use itertools::Itertools;
+
+fn corrupted(line: &str) -> Option<char> {
+    let mut stack = vec![];
+    for ch in line.chars() {
+        match ch {
+            '(' | '[' | '{' | '<' => {
+                stack.push(ch);
+            }
+            ')' | ']' | '}' | '>' => {
+                if let Some(c) = stack.pop() {
+                    match (c, ch) {
+                        ('(', ')') | ('[', ']') | ('{', '}') | ('<', '>') => {
+                            continue;
+                        }
+                        _ => {
+                            return Some(ch);
+                        }
+                    }
+                }
+            }
+            _ => panic!("Unexpected {ch}"),
+        }
+    }
+    None
+}
+
+fn incomplete_line(line: &str) -> Option<String> {
+    if corrupted(line).is_some() {
+        return None;
+    }
+    let mut stack = vec![];
+
+    for ch in line.chars() {
+        match ch {
+            '(' | '[' | '{' | '<' => {
+                stack.push(ch);
+            }
+            ')' | ']' | '}' | '>' => {
+                stack.pop();
+            }
+            _ => panic!("Unexpected {ch}"),
+        }
+    }
+    Some(stack.into_iter().rev().collect())
+}
+
+fn value(ch: char) -> usize {
+    match ch {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137,
+        _ => panic!("Not illegal {ch}"),
+    }
+}
+
+fn incomplete_value(s: String) -> usize {
+    let mut score = 0;
+    for ch in s.chars() {
+        score = score * 5
+            + match ch {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                _ => panic!("Unexpected {ch}"),
+            }
+    }
+    score
+}
+
+fn solve_1(input: &str) -> usize {
+    input.lines().filter_map(corrupted).map(value).sum()
+}
+
+fn solve_2(input: &str) -> usize {
+    let v = input
+        .lines()
+        .filter_map(incomplete_line)
+        .map(incomplete_value)
+        .sorted()
+        .collect_vec();
+    v[v.len() / 2]
+}
+
+pub fn part_1(input: &str) -> Result<()> {
+    let score = solve_1(input);
+    println!("{score}");
+    Ok(())
+}
+
+pub fn part_2(input: &str) -> Result<()> {
+    let score = solve_2(input);
+    println!("{score}");
+    Ok(())
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(solve_1(EXAMPLE), 26397);
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(solve_2(EXAMPLE), 288957);
+    }
+
+    const EXAMPLE: &str = "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+";
+}
--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ pub mod day_06;
 pub mod day_07;
 pub mod day_08;
 pub mod day_09;
+pub mod day_10;
 pub mod dl_data;
 
 pub struct Solution {
@@ -113,8 +114,8 @@ pub const SOLUTIONS: [Solution; 25] = [
     },
     Solution {
         day_no: 10,
-        part_1: not_implemented,
-        part_2: not_implemented,
+        part_1: day_10::part_1,
+        part_2: day_10::part_2,
     },
     Solution {
         day_no: 11,
