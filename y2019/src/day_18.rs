b07ff7c5b43e6ea97230bcdfaa30966755004bc8
Robin Kåveland <kaaveland@gmail.com>
2023-04-22 22:25:38 +0200

    Solve day 18 2019 part two

--- y2019/src/day_18.rs
+++ y2019/src/day_18.rs
@@ -79,15 +79,29 @@ impl State {
     }
 }
 
+fn next_state(came_from: &State, went_to: (i32, i32), tile: &Tile) -> Option<State> {
+    match tile {
+        Tile::Empty | Tile::PlayerStart => Some(State {
+            keys: came_from.keys,
+            loc: went_to,
+            step: came_from.step + 1,
+        }),
+        Tile::Door(ch) if came_from.keys.has(*ch) => Some(State {
+            keys: came_from.keys,
+            loc: went_to,
+            step: came_from.step + 1,
+        }),
+        Tile::Key(ch) => Some(State {
+            keys: came_from.keys.add_key(*ch),
+            loc: went_to,
+            step: came_from.step + 1,
+        }),
+        _ => None,
+    }
+}
+
 fn solve_maze(loc: (i32, i32), map: &Map) -> Result<usize> {
-    let key_bits = map
-        .values()
-        .filter_map(|tile| match tile {
-            Tile::Key(ch) => Some(1 << (*ch as u32 - 'a' as u32)),
-            _ => None,
-        })
-        .fold(0, |acc, x| acc | x);
-    let all_keys = Keys { have: key_bits };
+    let all_keys = all_keys(&map);
     let mut queue = VecDeque::new();
     queue.push_back(State::new(loc));
     let mut cache = HashSet::default();
@@ -102,28 +116,9 @@ fn solve_maze(loc: (i32, i32), map: &Map) -> Result<usize> {
             .iter()
             .map(|(dx, dy)| (x + dx, y + dy))
             .filter_map(|loc| map.get(&loc).map(|tile| (loc, tile)));
-        let keys = state.keys;
 
         for (loc, tile) in neighbours {
-            let next = match tile {
-                Tile::Empty | Tile::PlayerStart => Some(State {
-                    keys,
-                    loc,
-                    step: state.step + 1,
-                }),
-                Tile::Door(ch) if keys.has(*ch) => Some(State {
-                    keys,
-                    loc,
-                    step: state.step + 1,
-                }),
-                Tile::Key(ch) => Some(State {
-                    keys: keys.add_key(*ch),
-                    loc,
-                    step: state.step + 1,
-                }),
-                _ => None,
-            };
-            if let Some(next) = next {
+            if let Some(next) = next_state(&state, loc, tile) {
                 let cache_key = (next.keys, next.loc);
                 if cache.insert(cache_key) {
                     queue.push_back(next);
@@ -138,6 +133,118 @@ fn solve_maze(loc: (i32, i32), map: &Map) -> Result<usize> {
 pub fn part_1(input: &str) -> Result<String> {
     let (player_pos, map) = parse_map(input)?;
     solve_maze(player_pos, &map).map(|n| format!("{n}"))
+}
+
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
+struct RobotsState {
+    keys: Keys,
+    locs: [(i32, i32); 4],
+    step: usize,
+}
+
+impl RobotsState {
+    fn new(locs: [(i32, i32); 4]) -> Self {
+        Self {
+            keys: Keys::default(),
+            locs,
+            step: 0,
+        }
+    }
+}
+
+fn all_keys(map: &Map) -> Keys {
+    let key_bits = map
+        .values()
+        .filter_map(|tile| match tile {
+            Tile::Key(ch) => Some(1 << (*ch as u32 - 'a' as u32)),
+            _ => None,
+        })
+        .fold(0, |acc, x| acc | x);
+    Keys { have: key_bits }
+}
+
+fn solve_maze_2(loc: (i32, i32), map: &Map) -> Result<usize> {
+    let all_keys = all_keys(map);
+    // Transform the map like assignment describes:
+    // ...
+    // .@.
+    // ...
+    // becomes
+    // @#@
+    // ###
+    // @#@
+    let mut map = map.clone();
+    let (x, y) = loc;
+    map.remove(&(x, y));
+    map.remove(&(x - 1, y));
+    map.remove(&(x + 1, y));
+    map.remove(&(x, y - 1));
+    map.remove(&(x, y + 1));
+    let bots = [
+        (x - 1, y - 1),
+        (x + 1, y - 1),
+        (x - 1, y + 1),
+        (x + 1, y + 1),
+    ];
+    let mut queue = VecDeque::new();
+    queue.push_back(RobotsState::new(bots));
+    let mut cache = HashSet::default();
+
+    while let Some(state) = queue.pop_front() {
+        if state.keys == all_keys {
+            return Ok(state.step);
+        }
+
+        let bots = state.locs;
+        for (bot, (x, y)) in bots.iter().enumerate() {
+            let neighbours = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+                .iter()
+                .map(|(dx, dy)| (x + dx, y + dy))
+                .filter_map(|loc| map.get(&loc).map(|tile| (loc, tile)));
+
+            for (loc, tile) in neighbours {
+                let mut next_bots = bots;
+                next_bots[bot] = loc;
+                let next_state = match tile {
+                    Tile::Empty | Tile::PlayerStart => Some(RobotsState {
+                        keys: state.keys,
+                        locs: next_bots,
+                        step: state.step + 1,
+                    }),
+                    Tile::Door(ch) if state.keys.has(*ch) => Some(RobotsState {
+                        keys: state.keys,
+                        locs: next_bots,
+                        step: state.step + 1,
+                    }),
+                    Tile::Key(ch) => Some(RobotsState {
+                        keys: state.keys.add_key(*ch),
+                        locs: next_bots,
+                        step: state.step + 1,
+                    }),
+                    _ => None,
+                };
+                if let Some(next) = next_state {
+                    // If any previous state with the same keys moved this bot to its next location,
+                    // we can skip adding it to the search
+                    let cache_key = (next.keys, bot, next.locs[bot]);
+                    if cache.insert(cache_key) {
+                        queue.push_back(RobotsState {
+                            keys: next.keys,
+                            locs: next_bots,
+                            step: next.step,
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    Err(anyhow!("No solution found"))
+}
+
+pub fn part_2(input: &str) -> Result<String> {
+    let (player_pos, map) = parse_map(input)?;
+    solve_maze_2(player_pos, &map).map(|n| format!("{n}"))
 }
 
 #[cfg(test)]
@@ -177,5 +284,18 @@ mod tests {
 ";
         let (player_pos, map) = parse_map(input).unwrap();
         assert_eq!(solve_maze(player_pos, &map).unwrap(), 86);
+    }
+
+    #[test]
+    fn should_solve_part2_example() {
+        let input = "#######
+#a.#Cd#
+##...##
+##.@.##
+##...##
+#cB#Ab#
+#######";
+        let (player_pos, map) = parse_map(input).unwrap();
+        assert_eq!(solve_maze_2(player_pos, &map).unwrap(), 8);
     }
 }
--- y2019/src/lib.rs
+++ y2019/src/lib.rs
@@ -110,7 +110,7 @@ pub const SOLUTIONS: [Solution; 25] = [
     Solution {
         day_no: 18,
         part_1: day_18::part_1,
-        part_2: not_implemented,
+        part_2: day_18::part_2,
     },
     Solution {
         day_no: 19,
