0bf1490af414beb9f8726ce04ad9674e4e804d64
Robin Kåveland <kaaveland@gmail.com>
2024-12-23 00:17:26 +0100

    Make 2024 day 22 single-threaded fast

--- y2024/src/day_22.rs
+++ y2024/src/day_22.rs
@@ -1,7 +1,3 @@
-use fxhash::FxHashMap;
-use itertools::izip;
-use rayon::prelude::*;
-
 fn next_secret(mut secret: u64) -> u64 {
     secret = ((secret << 6) ^ secret) & 0xFFFFFF;
     secret = ((secret >> 5) ^ secret) & 0xFFFFFF;
@@ -25,64 +21,41 @@ pub fn part_1(inp: &str) -> anyhow::Result<String> {
     Ok(format!("{n}"))
 }
 
-fn gen_prices(secrets: &mut [u64], rounds: usize) -> Vec<Vec<i8>> {
-    let mut prices = vec![vec![]; secrets.len()];
-    secrets.iter().zip(prices.iter_mut()).for_each(|(s, p)| {
-        let dig = (s % 10) as i8;
-        p.push(dig);
-    });
-    for _ in 0..rounds {
-        secrets
-            .iter_mut()
-            .zip(prices.iter_mut())
-            .for_each(|(s, p)| {
-                *s = next_secret(*s);
-                let dig = (*s % 10) as i8;
-                p.push(dig);
-            })
+fn maximize_bananas(monkeys: &[i32]) -> i32 {
+    let mut counter = vec![0; 1 << 20];
+    for monkey in monkeys {
+        let mut secret = *monkey;
+        let mut last_price = secret % 10;
+        let mut d0;
+        let (mut d1, mut d2, mut d3) = (0u8, 0u8, 0u8);
+        let mut seen = vec![false; 1 << 20];
+
+        for round in 0..2000 {
+            secret = ((secret << 6) ^ secret) & 0xFFFFFF;
+            secret = ((secret >> 5) ^ secret) & 0xFFFFFF;
+            secret = ((secret << 11) ^ secret) & 0xFFFFFF;
+            let price = secret % 10;
+            (d0, d1, d2) = (d1, d2, d3);
+            d3 = (price - last_price + 9) as u8;
+            if round >= 3 {
+                let k = ((d0 & 0x1F) as i32)
+                    | (((d1 & 0x1F) as i32) << 5)
+                    | (((d2 & 0x1F) as i32) << 10)
+                    | (((d3 & 0x1F) as i32) << 15);
+                if !seen[k as usize] {
+                    seen[k as usize] = true;
+                    counter[k as usize] += price;
+                }
+            }
+            last_price = price;
+        }
     }
-
-    prices
-}
-
-fn most_bananas(prices: &[Vec<i8>]) -> i32 {
-    let m: FxHashMap<_, _> = prices
-        .into_par_iter()
-        .map(|p| {
-            let mut m = FxHashMap::default();
-            let deltas_0 = p
-                .iter()
-                .zip(p.iter().skip(1))
-                .map(|(a, b)| b - a)
-                .map(|n| (n + 9) as u8);
-            let deltas_1 = deltas_0.clone().skip(1);
-            let deltas_2 = deltas_1.clone().skip(1);
-            let deltas_3 = deltas_2.clone().skip(1);
-            let p4 = p.iter().skip(4);
-            for (d0, d1, d2, d3, p) in izip!(deltas_0, deltas_1, deltas_2, deltas_3, p4) {
-                let k = ((d0 as u32) & 0x1F)
-                    | (((d1 as u32) & 0x1F) << 5)
-                    | (((d2 as u32) & 0x1F) << 10)
-                    | (((d3 as u32) & 0x1F) << 15);
-                m.entry(k).or_insert(*p as i32);
-            }
-            m
-        })
-        .reduce(FxHashMap::default, |mut acc, map| {
-            for (k, v) in map {
-                *acc.entry(k).or_default() += v;
-            }
-            acc
-        });
-    *m.values().max().unwrap()
+    counter.into_iter().max().unwrap()
 }
 
 pub fn part_2(inp: &str) -> anyhow::Result<String> {
-    let secrets: anyhow::Result<Vec<u64>> = inp.lines().map(|n| Ok(n.parse::<u64>()?)).collect();
-    let mut secrets = secrets?;
-    let prices = gen_prices(&mut secrets, 2000);
-    let n = most_bananas(&prices);
-    Ok(format!("{n}"))
+    let monkeys: anyhow::Result<Vec<i32>> = inp.lines().map(|n| Ok(n.parse::<i32>()?)).collect();
+    Ok(format!("{}", maximize_bananas(&(monkeys?))))
 }
 
 #[cfg(test)]
@@ -91,9 +64,8 @@ mod tests {
 
     #[test]
     fn test_ex_part2() {
-        let mut secrets = vec![1, 2, 3, 2024];
-        let prices = gen_prices(&mut secrets, 2000);
-        assert_eq!(most_bananas(&prices), 23);
+        let secrets = vec![1, 2, 3, 2024];
+        assert_eq!(maximize_bananas(&secrets), 23);
     }
     #[test]
     fn test_next_secret() {
@@ -105,12 +77,6 @@ mod tests {
             n = next_secret(n);
             assert_eq!(next, n);
         }
-    }
-
-    #[test]
-    fn check_gen_prices() {
-        let p = gen_prices(&mut [123], 9);
-        assert_eq!(p[0], vec![3, 0, 6, 5, 4, 4, 6, 4, 4, 2]);
     }
 
     #[test]
