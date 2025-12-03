a2ec830f9e2241dfb989e75b353c7bdb345de625
Robin Kåveland <robin@kaveland.no>
2025-12-03 23:20:40 +0100

    Solve 2025 day 3

---
+++ y2025/src/day_03.rs
@@ -0,0 +1,55 @@
+use itertools::Itertools;
+use std::cmp::Reverse;
+
+// Obviously correct, but also very slow for 12 digits because it tries every possible combination
+fn max_joltage_by_digits(bank: &[u8], digits: usize, acum: u128) -> u128 {
+    if bank.len() < digits {
+        panic!("BUG: Not that many digits available")
+    };
+    if digits == 0 {
+        acum
+    } else {
+        // We need to use the last digits, pick the next one from the start of the string
+        let candidates = &bank[..bank.len() - digits + 1];
+
+        let (&digit, Reverse(index)) = candidates
+            .iter()
+            .enumerate()
+            // digit desc, index asc as tie-breaker
+            .map(|(index, digit)| (digit, Reverse(index)))
+            .max()
+            .expect("BUG: Not that many digits available in bank");
+
+        max_joltage_by_digits(
+            &bank[index + 1..],
+            digits - 1,
+            acum * 10 + ((digit - b'0') as u128),
+        )
+    }
+}
+
+fn total_joltage(s: &str, digits: usize) -> u128 {
+    s.lines()
+        .filter(|l| !l.is_empty())
+        .map(|l| l.as_bytes())
+        .map(|bank| max_joltage_by_digits(bank, digits, 0))
+        .sum()
+}
+
+pub fn part_1(s: &str) -> anyhow::Result<String> {
+    Ok(format!("{}", total_joltage(s, 2)))
+}
+
+pub fn part_2(s: &str) -> anyhow::Result<String> {
+    Ok(format!("{}", total_joltage(s, 12)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_max_joltage() {
+        assert_eq!(98, max_joltage_by_digits(b"987654321111111", 2, 0));
+    }
+}
--- y2025/src/lib.rs
+++ y2025/src/lib.rs
@@ -2,6 +2,7 @@ use shared::Answer;
 
 mod day_01;
 mod day_02;
+mod day_03;
 
 pub const SOLUTIONS: &[Answer] = &[
     Answer::SolvedBoth {
@@ -12,7 +13,10 @@ pub const SOLUTIONS: &[Answer] = &[
         part_1: day_02::part_1,
         part_2: day_02::part_2,
     },
-    Answer::NotImplementedYet,
+    Answer::SolvedBoth {
+        part_1: day_03::part_1,
+        part_2: day_03::part_2,
+    },
     Answer::NotImplementedYet,
     Answer::NotImplementedYet,
     Answer::NotImplementedYet,
